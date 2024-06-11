d2830742dbdaf63eed5b958722472b6309fc7b1f
James Hugman <james@hugman.tv>
2024-06-11 15:24:54 +0100

    Use android logcat

--- cpp/react-native-matrix-rust-sdk.cpp
+++ cpp/react-native-matrix-rust-sdk.cpp
@@ -1,4 +1,11 @@
 #include "react-native-matrix-rust-sdk.h"
+#include <android/log.h>
+
+#define LOG_TAG "MyRondpointApp"
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 
 namespace matrixrustsdk {
 	using namespace facebook;
@@ -7,6 +14,8 @@ namespace matrixrustsdk {
 	}
 
 	uint8_t installRustCrate(jsi::Runtime &runtime, uint8_t b) {
+		LOGI("About to try and install NativeRondpoint");
+		LOGI("Installed Rondpoint");
 		return false;
 	}
 
